21ddde7d26783373da8ab71275b6c18962003373
AbilioN <netobalby@gmail.com>
2022-09-01 09:18:19 +0100

    random generate enemies

--- app.js
+++ app.js
@@ -50,14 +50,65 @@ class Projectile {
         this.y = this.y + this.velocity.y
     }
 }
+class Enemy {
+    
+    constructor(x, y, radius, color, velocity)
+    {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
 
+    }
+
+    draw()
+    {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI *2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update()
+    {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
 const x = canvas.width/2
 const y = canvas.height/2
 const player = new Player(x, y, 30, 'blue')
 player.draw()
 
 const projectiles = []
+const enemies = []
 
+
+function spawnEnemies()
+{
+    setInterval(()=>{
+        const x = Math.random() * canvas.width
+        const y = Math.random() * canvas.height
+        const radius = 30
+        const color = 'green'
+
+        const angle =  Math.atan2(
+            canvas.height/2 - y, 
+            canvas.width/2 - x 
+            )
+
+        const velocity = {
+            x : 3 * Math.cos(angle),
+            y : 3 * Math.sin(angle)
+        }
+  
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+
+        console.log(enemies)
+    }, 1000)
+}
 function animate()
 {
     requestAnimationFrame(animate)
@@ -66,15 +117,22 @@ function animate()
     projectiles.forEach(projectile => {
         projectile.update()
     })
+
+    enemies.forEach(enemy => {
+        enemy.update()
+    })
 }
 
 window.addEventListener('click', (event)=>{
 
-    const angle =  Math.atan2(event.clientY - canvas.height/2, event.clientX - canvas.width/2)
+    const angle =  Math.atan2(
+        event.clientY - canvas.height/2,
+        event.clientX - canvas.width/2
+        )
 
     const velocity = {
-        x : 5 * Math.cos(angle),
-        y : 5 * Math.sin(angle)
+        x : 10 * Math.cos(angle),
+        y : 10 * Math.sin(angle)
     }
     projectiles.push(new Projectile(
         canvas.width/2,
@@ -85,4 +143,5 @@ window.addEventListener('click', (event)=>{
     ))
 })
 
+spawnEnemies()
 animate()
